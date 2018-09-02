cf7dfe163486d38ba1a2a2a2c99c5224645a664a
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-09-02 19:17:30 -0300

    Povoando dinamicamento cidade e estado

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { EstadoService } from '../../services/domain/estado.service';
+import { CidadeService } from './../../services/domain/cidade.service';
+import { EstadoDTO } from './../../models/Estado.dto';
+import { CidadeDTO } from './../../models/cidade.dto';
 
 @IonicPage()
 @Component({
@@ -11,36 +15,58 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 export class SignupPage {
 
   formGroup: FormGroup;
+  estados: EstadoDTO[];
+  cidades: CidadeDTO[];
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public formBuilder: FormBuilder) {
+    public formBuilder: FormBuilder,
+    public cidadeService: CidadeService,
+    public estadoService: EstadoService) {
 
-      this.formGroup = this.formBuilder.group({
-        nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
-        email: ['', [Validators.required,Validators.email]],
-        tipo: ['', [Validators.required]],
-        cpfOuCnpj: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-        senha: ['', [Validators.required, Validators.maxLength(120)]],
-        logradouro: ['', [Validators.required, Validators.maxLength(120)]],
-        numero: ['', [Validators.required]],
-        complemento: ['', [Validators.maxLength(120)]],
-        bairro: ['', [Validators.required]],
-        cep: ['', [Validators.required]],
-        telefone1: ['', [Validators.required]],
-        telefone2: ['', []],
-        telefone3: ['', []],
-        estadoId: [null, [Validators.required]],
-        cidadeId: [null, [Validators.required]],
-      });
+    this.formGroup = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['', [Validators.required, Validators.email]],
+      tipo: ['', [Validators.required]],
+      cpfOuCnpj: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+      senha: ['', [Validators.required, Validators.maxLength(120)]],
+      logradouro: ['', [Validators.required, Validators.maxLength(120)]],
+      numero: ['', [Validators.required]],
+      complemento: ['', [Validators.maxLength(120)]],
+      bairro: ['', [Validators.required]],
+      cep: ['', [Validators.required]],
+      telefone1: ['', [Validators.required]],
+      telefone2: ['', []],
+      telefone3: ['', []],
+      estadoId: [null, [Validators.required]],
+      cidadeId: [null, [Validators.required]],
+    });
+  }
+
+  ionViewDidLoad() {
+    this.estadoService.findAll()
+      .pipe()
+      .subscribe(resp => {
+        this.estados = resp;
+        this.formGroup.controls.estadoId.setValue(this.estados[0].id);
+        this.updateCidades();
+      },
+        error => { });
+  }
+
+  updateCidades() {
+    let estado_id = this.formGroup.value.estadoId;
+    this.cidadeService.findAll(estado_id)
+      .pipe()
+      .subscribe(resp => {
+        this.cidades = resp;
+        this.formGroup.controls.cidadeId.setValue(null);
+      },
+        error => { });
   }
 
   signupUser() {
     console.log("enviou")
-  }
-
-  updateCidades() {
-
   }
 
 }
