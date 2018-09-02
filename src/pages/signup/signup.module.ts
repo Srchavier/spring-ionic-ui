a7d5fa7036aa41b3e4b8739fb3d8a0fd99f753e6
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-09-02 19:46:55 -0300

    Mostrando erros de validacao

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupPage } from './signup';
+import { BrMaskerModule } from 'brmasker-ionic-3';
 
 import { CidadeService } from './../../services/domain/cidade.service';
 import { EstadoService } from './../../services/domain/estado.service';
@@ -8,13 +9,15 @@ import { EstadoService } from './../../services/domain/estado.service';
 @NgModule({
   declarations: [
     SignupPage,
+
   ],
   imports: [
     IonicPageModule.forChild(SignupPage),
+    BrMaskerModule
   ],
   providers: [
     EstadoService,
     CidadeService
   ]
 })
-export class SignupPageModule {}
+export class SignupPageModule { }
