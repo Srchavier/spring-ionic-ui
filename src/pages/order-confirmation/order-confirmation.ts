fe98f0a3a377747f2799463351bd50f27f6971b2
Eduardo Rodrigues <eduardug.tinho@hotmail.com>
2018-09-05 22:40:23 -0300

    Informando pedido registrado ao usuário

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -20,6 +20,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codigoPedido: string;
 
   constructor(
     public navCtrl: NavController,
@@ -55,16 +56,25 @@ export class OrderConfirmationPage {
     this.navCtrl.setRoot('CartPage');
   }
 
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
   checkout() {
     this.pedidoService.insert(this.pedido)
       .subscribe(resp => {
         this.cartService.createOrClearCart();
-        console.log(resp.headers.get('location'));
+        this.codigoPedido = this.extractId(resp.headers.get('location'));
       },
         error => {
           if (error.status == 403) {
             this.navCtrl.setRoot('HomePage')
           }
         })
+  }
+
+  private extractId(location: string): string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
